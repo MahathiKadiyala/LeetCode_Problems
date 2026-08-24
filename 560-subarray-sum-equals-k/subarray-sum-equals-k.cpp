4b68771311a166cf385be856713833e849ff0eff
MahathiKadiyala <mahathikadiyala07@gmail.com>
2026-08-24 15:39:47 +0530

    Time: 40 ms (65.54%) | Memory: 45.5 MB (55.48%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,19 +2,30 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<int>p(n+1,0);
-        // for(int i=0;i<n;i++){
-        //     p[i+1]=p[i]+nums[i];
-        // }
-        for(int i=1;i<=n;i++){
-            p[i]=p[i-1]+nums[i-1];
-        }
         int cnt=0;
-        for(int i=0;i<p.size();i++){
-            for(int j=i+1;j<p.size();j++){
-                if((p[j]-p[i])==k) cnt++;
-            }
+        unordered_map<int,int>m;
+        m[0]++;
+        int ps=0;
+        for(int x:nums){
+            ps+=x;
+            int r=ps-k;
+            if(m.find(r)!=m.end()) cnt+=m[r];
+            m[ps]++;
         }
         return cnt;
+         // vector<int>p(n+1,0);
+        // for(int i=1;i<=n;i++){
+        //     p[i]=p[i-1]+nums[i-1];
+        // }
+        // int cnt=0;
+        // for(int i=0;i<p.size();i++){
+        //     for(int j=i+1;j<p.size();j++){
+        //         if((p[j]-p[i])==k) cnt++;
+        //     }
+        // }
+        //return cnt;
     }
 };
+// for(int i=0;i<n;i++){
+        //     p[i+1]=p[i]+nums[i];
+        // }

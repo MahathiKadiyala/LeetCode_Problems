c5ad4045c7daef6467cfddc710f2fc2cc77a7976
MahathiKadiyala <mahathikadiyala07@gmail.com>
2026-09-19 20:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (12.39%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<int>row(m,0);
+        vector<int>col(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){ 
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+              if(row[i]==1 || col[j]==1){
+                matrix[i][j]=0;
+              }
+            }
+        }
+    }
+};
+
+// --
+// int m=matrix.size();
+//         int n=matrix[0].size();
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(matrix[i][j]==0){
+//                     for(int col=0;col<n;col++) {
+//                         if(matrix[i][col]!=0) matrix[i][col]=INT_MIN;
+//                     }
+//                     for(int row=0;row<m;row++){
+//                         if(matrix[row][j]!=0) matrix[row][j]=INT_MIN;
+//                     }
+//                 }
+//             }
+//         }
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(matrix[i][j]==INT_MIN) matrix[i][j]=0;
+//             }
+//         }

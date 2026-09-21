80b26565adebb8659fad6b61b3f6f30e1ff24602
MahathiKadiyala <mahathikadiyala07@gmail.com>
2026-09-21 06:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (54.12%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+       //next_permutation(nums.begin(),nums.end()); 
+        int n=nums.size();
+        int i=n-2;
+    while(i>=0 && nums[i]>=nums[i+1]){ // find pivot
+        i--;
+    }
+    if(i>=0){
+        int j=n-1;
+        while(nums[j]<=nums[i]) {  // find successor
+            j--;
+        }
+        swap(nums[i], nums[j]);
+    }
+    reverse(nums.begin()+i+1,nums.end());
+    }
+};
